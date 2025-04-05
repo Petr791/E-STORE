614b7a1c0ed5c62cf3ce4f446fdb252410ede34c
Petr <petr777c@gmail.com>
2025-04-05 21:50:07 +0300

    Update constants.js

--- js/constants.js
+++ js/constants.js
@@ -1,25 +1,3 @@
-/* export function setValue() {
-	const USER_ID_STRING = 'userID';
-	const CART_STRING = 'cart';
-
-	const CATALOG_PAGE_URL = '../index.html';
-	const ORDER_PAGE_URL = '/E-STORE/pages/order.html';
-	const CART_PAGE_URL = './cart.html';
-
-	const ERROR_CATALOG_STRING = 'Ошибка загрузки каталога';
-	const ERROR_PRODUCT_STRING = 'Упс, ошибка. Такого товара нет.';
-	const ERROR_CART_STRING = 'Корзина пуста';
-	const ERROR_CHECKOUT_STRING = 'В заказе нет товаров';
-	const ERROR_ORDER_STRING = 'Страница не найдена';
-
-	const CART_TITLE_STRING = 'Проверь корзину';
-
-	const POPUP_OPENED_CLASSNAME = 'popup_open';
-	const BODY_FIXED_CLASSNAME = 'body_fixed';
-
-	const DELIVERY_PRICE_NUMBER = 6.99;
-} */
-
 export const USER_ID_STRING = 'userID';
 export const CART_STRING = 'cart';
 
